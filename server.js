e4bb79050d0ca9f549b5c0644042c58d61f910d3
Thomas Attermann <thomas_5009@hotmail.com>
2017-11-16 15:09:26 +0100

    Setup bcrypt.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies
-var express = require('express');
+const express = require('express');
 const BodyParser = require('body-parser');
-var ObjectId = require('mongodb').ObjectID;
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+const ObjectId = require('mongodb').ObjectID;
+const MongoClient = require('mongodb').MongoClient
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 app = express();
 app.use(BodyParser.urlencoded({
     extended: true
@@ -12,9 +13,10 @@ app.use(BodyParser.json());
 
 var database;
 
-// Connection URL
+// --------------------------------
+// Setup connection to databse
+// --------------------------------
 var url = 'mongodb://localhost:27017/cryptoexam';
-// Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
     if (err) {
         console.log('An error occured while connecting to the database')
@@ -22,36 +24,51 @@ MongoClient.connect(url, function(err, db) {
         console.log("Connected correctly to server");
         database = db
     }
-    //db.close();
 });
 
 
-
+// --------------------------------
+// CREATE USER
+// --------------------------------
 
 app.post('/user/', function (req, res) {
 
+    // Create variable containing body from request (user information)
     var user = req.body;
 
+    // Create reference to the users correction in the database.
     var collection = database.collection('users');
 
-    collection.insertOne( user, function(err, result) {
-        if (err) {
-            return res.json({error: "Failure"})
-        } else {
-            return res.json({success:"Inserted user into DB", user: user})
-        }
-    });
+    // Generate salt to be used in hash function.
+    bcrypt.genSalt(saltRounds, function(err, salt) {
 
+        // Hash users password using generated salt.
+        bcrypt.hash(user.password, salt, function(err, hash) {
+
+            // Assign user password to generated hash - Key deriviation function.
+            user.password = hash;
+
+            // Insert user document into database
+            collection.insertOne( user, function(err, result) {
+                if (err) {
+                    return res.json({error: "Internal failure", error: err})
+                } else {
+                    user.password = undefined;
+                    return res.json({success:"Inserted user into DB", user: user})
+                    db.close();
+                }
+            });
+        });
+    });
 })
 
+// --------------------------------
+// LOGIN
+// --------------------------------
 
+app.post('/login/', function (req, res) {
 
-
-
-
-
-
-
+})
 
 
 app.listen(process.env.PORT || 4000);
