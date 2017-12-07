bc3d5724da9dc616fa2639b2cb07b9aeb46df7e6
Thomas Attermann <thomas_5009@hotmail.com>
2017-12-07 15:06:26 +0100

    Cleanup

--- verifytoken.js
+++ verifytoken.js
@@ -5,7 +5,7 @@ function verifyToken(req, res, next) {
     // verify a token asymmetric
     var token = req.headers['authorization']
     var publicKey = process.env.API_RSA_PUB // get public key
-    //var publicKey = fs.readFileSync('api.rsa.pub');  // get public key
+
     jwt.verify(token, publicKey, { algorithms: ['RS256'] }, function (err, payload) {
         if (err) {
             return res.status(401).json({ error: "Invalid token" })
